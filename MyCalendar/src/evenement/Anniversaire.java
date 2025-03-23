1aa5ac5f6b07b9bbea961ec8542b3b455a1484a7
matheo <matheo.pedron57@gmail.com>
2025-03-23 14:16:27 +0100

    Validation des tests (vert)

--- MyCalendar/src/evenement/Anniversaire.java
+++ MyCalendar/src/evenement/Anniversaire.java
@@ -4,15 +4,30 @@ import objet.Periode;
 import objet.Personne;
 import objet.Titre;
 
-// TODO implementer la logique
-public class Anniversaire extends Evenement {
+import java.time.LocalDateTime;
 
-	public Anniversaire(Titre titre, Personne proprietaire, Periode periode) {
+public class Anniversaire extends Evenement {
+	private Personne personne_fetee;
+
+	public Anniversaire(Titre titre, Personne proprietaire, Periode periode, Personne personne_fetee) {
 		super(titre, proprietaire, periode);
+		this.personne_fetee = personne_fetee;
+	}
+
+	@Override
+	public boolean evenementDansPeriode(Periode periode) {
+		LocalDateTime temp = this.periode.dateDebut();
+		while (temp.isBefore(periode.dateFin())) {
+			if (!temp.isBefore(periode.dateDebut())) {
+				return true;
+			}
+			temp = temp.plusYears(1);
+		}
+		return false;
 	}
 
 	@Override
 	public String description() {
-		return "";
+		return "Anniversaire de " + personne_fetee + " le " + periode.dateDebut().toLocalDate();
 	}
 }
--- MyCalendar/test/evenement/AnniversaireTest.java
+++ MyCalendar/test/evenement/AnniversaireTest.java
@@ -13,7 +13,7 @@ public class AnniversaireTest {
 	void anniversaire_zero_jour() {
 		Periode periode1 = new Periode(LocalDateTime.of(2025, 3, 2, 0, 0), 60*24);
 		Periode periode2 = new Periode(LocalDateTime.of(2025, 3, 1, 0, 0), 60*24*2);
-		Anniversaire anniversaire = new Anniversaire(null, null, periode1);
+		Anniversaire anniversaire = new Anniversaire(null, null, periode1, null);
 
 		assertTrue(anniversaire.evenementDansPeriode(periode2));
 	}
@@ -22,7 +22,7 @@ public class AnniversaireTest {
 	void anniversaire_un_an() {
 		Periode periode1 = new Periode(LocalDateTime.of(2025, 3, 2, 0, 0), 60*24);
 		Periode periode2 = new Periode(LocalDateTime.of(2026, 3, 1, 0, 0), 60*24*2);
-		Anniversaire anniversaire = new Anniversaire(null, null, periode1);
+		Anniversaire anniversaire = new Anniversaire(null, null, periode1, null);
 
 		assertTrue(anniversaire.evenementDansPeriode(periode2));
 	}
@@ -30,8 +30,8 @@ public class AnniversaireTest {
 	@Test
 	void anniversaire_moins_un_an() {
 		Periode periode1 = new Periode(LocalDateTime.of(2025, 3, 2, 0, 0), 60*24);
-		Periode periode2 = new Periode(LocalDateTime.of(2026, 3, 1, 0, 0), 60*24*2);
-		Anniversaire anniversaire = new Anniversaire(null, null, periode1);
+		Periode periode2 = new Periode(LocalDateTime.of(2024, 3, 1, 0, 0), 60*24*2);
+		Anniversaire anniversaire = new Anniversaire(null, null, periode1, null);
 
 		assertFalse(anniversaire.evenementDansPeriode(periode2));
 	}
